5ae3969e4d05511d4c6b0a3c4183a31fbcfcb0a9
Mike Fikes <mike@fikesfarm.com>
2017-04-28 23:04:13 -0400

    Don’t let if constant or symbol

--- src/vector/core.clj
+++ src/vector/core.clj
@@ -5,37 +5,55 @@
   ([] 'vector.core/zero)
   ([v1] v1)
   ([v1 v2]
-   `(let [a# ~v1
-          b# ~v2]
-      (->Vector (cljs.core/+ (.-x a#) (.-x b#))
-                (cljs.core/+ (.-y a#) (.-y b#)))))
+   (if (and (symbol? v1)
+            (symbol? v2))
+     `(->Vector (cljs.core/+ (.-x ~v1) (.-x ~v2))
+                (cljs.core/+ (.-y ~v1) (.-y ~v2)))
+     `(let [a# ~v1
+            b# ~v2]
+        (->Vector (cljs.core/+ (.-x a#) (.-x b#))
+                  (cljs.core/+ (.-y a#) (.-y b#))))))
   ([v1 v2 & more]
    `(+ (+ ~v1 ~v2) ~@more)))
 
 (defmacro -
   ([v1]
-   `(let [a# ~v1]
-      (->Vector (cljs.core/- (.-x a#))
-                (cljs.core/- (.-y a#)))))
+   (if (symbol? v1)
+     `(->Vector (cljs.core/- (.-x ~v1))
+                (cljs.core/- (.-y ~v1)))
+     `(let [a# ~v1]
+        (->Vector (cljs.core/- (.-x a#))
+                  (cljs.core/- (.-y a#))))))
   ([v1 v2]
-   `(let [a# ~v1
-          b# ~v2]
-      (->Vector (cljs.core/- (.-x a#) (.-x b#))
-                (cljs.core/- (.-y a#) (.-y b#)))))
+   (if (and (symbol? v1)
+            (symbol? v2))
+     `(->Vector (cljs.core/- (.-x ~v1) (.-x ~v2))
+                (cljs.core/- (.-y ~v1) (.-y ~v2)))
+     `(let [a# ~v1
+            b# ~v2]
+        (->Vector (cljs.core/- (.-x a#) (.-x b#))
+                  (cljs.core/- (.-y a#) (.-y b#))))))
   ([v1 v2 & more]
    `(- (- ~v1 ~v2) ~@more)))
 
 (defmacro scale
   [k v]
-  `(let [k# ~k
-         v# ~v]
-    (->Vector (* k# (.-x v#)) (* k# (.-y v#)))))
+  (if (and (or (number? k)
+               (symbol? k))
+           (symbol? v))
+    `(->Vector (* ~k (.-x ~v)) (* ~k (.-y ~v)))
+    `(let [k# ~k
+           v# ~v]
+       (->Vector (* k# (.-x v#)) (* k# (.-y v#))))))
 
 (defmacro length
   [v]
-  `(let [v# ~v]
-     (~'Math/sqrt (cljs.core/+ (* (.-x v#) (.-x v#))
-                                (* (.-y v#) (.-y v#))))))
+  (if (symbol? v)
+    `(~'Math/sqrt (cljs.core/+ (* (.-x ~v) (.-x ~v))
+                               (* (.-y ~v) (.-y ~v))))
+    `(let [v# ~v]
+       (~'Math/sqrt (cljs.core/+ (* (.-x v#) (.-x v#))
+                                 (* (.-y v#) (.-y v#)))))))
 
 (defmacro mod
   [v1 v2]
